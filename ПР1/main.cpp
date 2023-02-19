5d1409d0ba7b7a30048841e4d7e42806772652f1
N0tTryHard <125365266+N0tTryHard@users.noreply.github.com>
2023-02-19 10:25:11 +0300

    Create main.cpp

---
+++ ПР1/main.cpp
@@ -0,0 +1,23 @@
+#include "triangle.h"
+#include <iostream>
+
+int main() {
+    Triangle mas[3];
+    double a, b, c;
+    for (int i = 0; i < 3; i++) {
+        std::cout << "Введите a, b и c для треугольника № " << i + 1 << " через пробел: " << std::endl;
+        std::cin >> a >> b >> c;
+        mas[i].set(a, b, c);
+        if (!(mas[i].exst_tr())) {
+            mas[i].show();
+            std::cout << "Треугольника с такими сторонами не существует, попробуйте ещё раз" << std::endl;
+            i--;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        mas[i].show();
+        std::cout << "Периметр треугольника: " << mas[i].perimetr() << std::endl;
+        std::cout << "Площадь треугольника: " << mas[i].square() << std::endl;
+    }
+    return 0;
+}
