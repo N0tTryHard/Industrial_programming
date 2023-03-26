4fe9e7e9b0972f54b7069ba3ab3ac49a2054f5ca
N0tTryHard <125365266+N0tTryHard@users.noreply.github.com>
2023-03-26 16:00:45 +0300

    Create Cone.h

---
+++ ПР6/#2/Cone.h
@@ -0,0 +1,32 @@
+//
+// Created by Господин on 14.03.2023.
+//
+
+#ifndef STUDY_CONE_H
+#define STUDY_CONE_H
+
+class Cone {
+protected:
+    double x, y, z;
+
+    double radius;
+    double height;
+
+public:
+    Cone();
+
+    Cone(double _radius, double _height);
+
+    Cone(double _x, double _y, double _z, double _radius, double _height);
+
+    void Set(double _x, double _y, double _z, double _radius, double _height);
+
+    void Print();
+
+    double Area();
+
+    double Volume();
+};
+
+
+#endif //STUDY_CONE_H
